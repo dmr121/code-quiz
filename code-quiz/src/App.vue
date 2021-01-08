91b0f32e5523c6aa6ad8cd209b906619f9f8555c
David Rozmajzl <davidrozmajzl@davids-mbp.lan>
2021-01-07 22:21:11 -0500

    Create grid with mockup images

--- code-quiz/src/App.vue
+++ code-quiz/src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div class="flex justify-center">
+    <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 py-3">
+      <img
+        src="./assets/images/man_with_wine.png"
+        alt="Man looking at camera behind some bottles of wine"
+        class=""
+      />
+      <img
+        src="./assets/images/man_on_beach.png"
+        alt="Man with sunglasse and a hat sitting on a beach"
+        class=""
+      />
+      <img
+        src="./assets/images/woman_pouring_wine.png"
+        alt="Woman pouring wine"
+        class=""
+      />
+      <img
+        src="./assets/images/black_lab_on_beach.png"
+        alt="Close-up of a black lab on a beach"
+        class=""
+      />
+      <img
+        src="./assets/images/woman_sitting_in_chair.png"
+        alt="Woman leaning on arm and smiling at camera"
+        class=""
+      />
+      <img
+        src="./assets/images/man_with_black_lab.png"
+        alt="Man playing with a black lab"
+        class=""
+      />
+      <img
+        src="./assets/images/man_holding_baby.png"
+        alt="Man holding a baby"
+        class=""
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
