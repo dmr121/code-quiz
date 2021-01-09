a8ec867c38f2acabfcf0115b83b750c4d509a010
David Rozmajzl <davidrozmajzl@davids-mbp.lan>
2021-01-08 22:06:03 -0500

    Move images to GridImages component

---
+++ code-quiz/src/components/GridImages.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex justify-center">
+    <div
+      class="grid gap-y-10 gap-x-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 py-3 px-3"
+    >
+      <img
+        src="./../assets/images/man_with_wine.png"
+        alt="Man looking at camera behind some bottles of wine"
+        class="grid-img"
+      />
+      <img
+        src="./../assets/images/man_on_beach.png"
+        alt="Man with sunglasse and a hat sitting on a beach"
+        class="grid-img"
+      />
+      <img
+        src="./../assets/images/woman_pouring_wine.png"
+        alt="Woman pouring wine"
+        class="grid-img"
+      />
+      <img
+        src="./../assets/images/black_lab_on_beach.png"
+        alt="Close-up of a black lab on a beach"
+        class="grid-img"
+      />
+      <img
+        src="./../assets/images/woman_sitting_in_chair.png"
+        alt="Woman leaning on arm and smiling at camera"
+        class="grid-img"
+      />
+      <img
+        src="./../assets/images/man_with_black_lab.png"
+        alt="Man playing with a black lab"
+        class="grid-img"
+      />
+      <img
+        src="./../assets/images/man_holding_baby.png"
+        alt="Man holding a baby"
+        class="grid-img"
+      />
+    </div>
+  </div>
+</template>
+
+<style>
+.grid-img {
+  @apply w-80;
+  @apply h-107;
+}
+</style>
