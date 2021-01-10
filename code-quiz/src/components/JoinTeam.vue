73f1a9d96d3d72c250977f64fac183f219fd11ee
David Rozmajzl <davidrozmajzl@davids-mbp.lan>
2021-01-09 19:52:25 -0500

    Display main text for component

---
+++ code-quiz/src/components/JoinTeam.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid grid-item bg-bluegray-50">
+    <div
+      class="self-center justify-self-center text-center font-semibold text-bluegray-700 font-sans pb-10"
+    >
+      <p class="text-4xl tracking-widest px-4 pb-6">
+        {{ toptextUpperCase }}
+      </p>
+      <p class="text-base font-josefin tracking-evenwider px-4">
+        {{ bottomtextUpperCase }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["toptext", "bottomtext"],
+  computed: {
+    toptextUpperCase() {
+      return this.toptext.toUpperCase();
+    },
+    bottomtextUpperCase() {
+      return this.bottomtext.toUpperCase();
+    },
+  },
+};
+</script>
